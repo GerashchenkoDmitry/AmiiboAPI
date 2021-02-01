0c778409e85747196e5302274f5dd14f2e6e3d99
Dmitry Gerashchenko <finiqw@gmail.com>
2021-02-01 18:58:33 +0700

    Initial Commit

---
+++ AmiiboTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmiiboTableView
+//
+//  Created by Дмитрий Геращенко on 01.02.2021.
+//  Copyright © 2021 Дмитрий Геращенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
